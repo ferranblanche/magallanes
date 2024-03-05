5dde05144d13c9577504b269029e3d8da32b3eb4
Ferran Blanche Xandri <ferran.blanche@gmail.com>
2024-03-05 23:46:52 +0100

    build: :art: Add formatter

--- apis/flights/src/index.ts
+++ apis/flights/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("🛩️ Flights API");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('🛩️ Flights API')
+})
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
--- apis/hotels/src/index.ts
+++ apis/hotels/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express'
 
-const app = express();
-const port = process.env.PORT || 3001;
+const app = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("🛌 Hotels API");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('🛌 Hotels API')
+})
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
